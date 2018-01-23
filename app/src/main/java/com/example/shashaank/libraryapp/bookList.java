fa36905c40812fabe77a53c5431585f23c0be6a1
Shashaank Srivastava <Shankisri@gmail.com>
2018-01-23 08:08:10 -0500

    comments

--- app/src/main/java/com/example/shashaank/libraryapp/bookList.java
+++ app/src/main/java/com/example/shashaank/libraryapp/bookList.java
@@ -10,6 +10,7 @@ public class bookList {
     private String _user;
     private boolean _location;
     String x = "";
+    // declares variables
 
     // constructor to assign values using paremters
     public bookList(String book, String _author, String _user){
@@ -19,6 +20,7 @@ public class bookList {
         this._location = true;
 
     }
+    // methods to return each of the assigned variables
     public String get_book() {
         return this._book;
     }
@@ -28,6 +30,7 @@ public class bookList {
     public String get_user() {
         return this._user;
     }
+    // method to return book location, location is automatically true when created
     public String get_location() {
         if (this._location == true){
             x= "Available";
